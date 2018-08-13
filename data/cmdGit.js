67dc99d9e94ca3a48806890fee6f07a9c7a63917
Jasper Wu <jasper.wu@cargosmart.com>
2018-08-13 17:51:41 +0800

    update cmdGit.js

--- data/cmdGit.js
+++ data/cmdGit.js
@@ -1,14 +1,22 @@
 let data = [{
-    option: '[创建并]切换至dev分支',
-    cmd: 'git checkout [-b] dev'
+    option: '[创建并]切换至dev分支[，并追踪远程分支]',
+    cmd: 'git checkout [-b] dev [o/dev]'
   },
   {
-    option: 'merge 目标分支的提交到当前分支',
+    option: '追踪远程分支 方式二, 如果当前分支就是dev，那么后面可不填',
+    cmd: 'git branch -u o/dev [dev]'
+  },
+  {
+    option: 'merge 目标分支提交到当前分支',
     cmd: 'git merge branchXXX'
   },
   {
     option: 'rebase 到目标分支',
     cmd: 'git rebase branchXXX'
+  },
+  {
+    option: 'rebase B 到 A 的下方,并且HEAD指向B',
+    cmd: 'git rebase A B'
   },
   {
     option: '让当前分支的HEAD指向本分支的前面两个提交',
@@ -46,6 +54,14 @@ let data = [{
     option: '查看离当前分支最近的带tag的commit',
     cmd: 'git describe'
   },
+  {
+    option: '拉取当前分支的远程提交，并且rebase于远程提交',
+    cmd: 'git pull --rebase'
+  },
+  {
+    option: 'git push <remote> <place>',
+    cmd: ''
+  }
 ]
 
 module.exports.data = data;
