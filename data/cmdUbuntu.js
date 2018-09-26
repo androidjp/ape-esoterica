45a872b03ad12754f08fa80a3c6ade06dd03dbed
Jasper Wu <jasper.wu@cargosmart.com>
2018-09-26 15:37:13 +0800

    add ubuntu apt registry

--- data/cmdUbuntu.js
+++ data/cmdUbuntu.js
@@ -12,6 +12,24 @@ let data = [
     cmd: 'sudo apt-get update'
   },
   {
+    option: 'apt设置阿里镜像',
+    isCmdList: true,
+    cmd: [
+      {
+        option: 'backup sources.list',
+        cmd: 'sudo cp /etc/apt/sources.list /etc/apt/sources.list_bak'
+      },
+      {
+        option: '添加这一段（updateDate：2018-09-11）',
+        cmd: 'http://wiki.ubuntu.org.cn/%E6%BA%90%E5%88%97%E8%A1%A8'
+      },
+      {
+        option: 'update apt 源',
+        cmd: 'sudo apt-get update'
+      }
+    ]
+  },
+  {
     option:'环境变量配置',
     isCmdList: true,
     cmd: [
